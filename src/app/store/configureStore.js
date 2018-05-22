26ec6c5a5fd9b53a7af12d60c16f5aa7be28de76
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2018-05-22 19:52:41 +0600

    Configure app store with middlewares & enable HMR for redux

---
+++ src/app/store/configureStore.js
@@ -0,0 +1,24 @@
+import { createStore, applyMiddleware } from "redux"
+import { composeWithDevTools } from "redux-devtools-extension"
+
+import rootReducer from "../reducers"
+
+export const configureStore = preloadedState => {
+  const middlewares = []
+  const middlewareEnhancer = applyMiddleware(...middlewares)
+  const composedEnhancer = composeWithDevTools(middlewareEnhancer)
+
+  const store = createStore(rootReducer, preloadedState, composedEnhancer)
+
+  // Enable hot module replacement
+  if (process.env.NODE_ENV !== "production") {
+    if (module.hot) {
+      module.hot.accept("../reducers", () => {
+        const newRootReducer = require("../reducers").default
+        store.replaceReducer(newRootReducer)
+      })
+    }
+  }
+
+  return store
+}
