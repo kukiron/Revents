abaa31a654a27d4baac68980b2b8a1a6224b5695
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2018-05-22 19:56:13 +0600

    Add separate home component

---
+++ src/features/home/index.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+const Home = ({ history }) => (
+  <div>
+    <div className="ui inverted vertical masthead center aligned segment">
+      <div className="ui text container">
+        <h1 className="ui inverted stackable header">
+          <img className="ui image massive" src="/assets/logo.png" alt="logo" />
+          <div className="content">Re-vents</div>
+        </h1>
+        <h2>Do whatever you want to do</h2>
+        <div
+          onClick={() => history.push("/events")}
+          className="ui huge white inverted button"
+        >
+          Get Started
+          <i className="right arrow icon" />
+        </div>
+      </div>
+    </div>
+  </div>
+)
+
+export default Home
