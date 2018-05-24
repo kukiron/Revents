d24611c4433337d191f7dabb5b8fdadb26eb75b6
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2018-05-25 00:12:35 +0600

    Static sidebar

---
+++ src/features/events/EventActivity/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { Header, Segment } from "semantic-ui-react"
+
+const EventActivity = () => (
+  <div>
+    <Header attached="top" content="Recent Activity" />
+    <Segment attached>
+      <p>Recent User Activity</p>
+    </Segment>
+  </div>
+)
+
+export default EventActivity
