89b958df7079b6b0bd70064835166093507f0b2c
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2018-05-29 21:41:07 +0600

    Add reducers for firebase & firestore

--- src/app/reducers/index.js
+++ src/app/reducers/index.js
@@ -1,5 +1,7 @@
 import { combineReducers } from "redux"
 import { reducer as formReducer } from "redux-form"
+import { firebaseReducer } from "react-redux-firebase"
+import { firestoreReducer } from "redux-firestore"
 import { reducer as toastrReducer } from "react-redux-toastr"
 
 import testReducer from "../../features/testArea/testReducer"
@@ -15,7 +17,9 @@ const rootReducer = combineReducers({
   modals: modalReducer,
   auth: authReducer,
   async: asyncReducer,
-  toastr: toastrReducer
+  toastr: toastrReducer,
+  firebase: firebaseReducer,
+  firestore: firestoreReducer
 })
 
 export default rootReducer
