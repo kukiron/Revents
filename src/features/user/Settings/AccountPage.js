7aaaf04da4656260f3e9910bd549aa300ec491e6
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2018-05-29 21:52:45 +0600

    AccountPage with password update options

--- src/features/user/Settings/AccountPage.js
+++ src/features/user/Settings/AccountPage.js
@@ -1,5 +1,107 @@
 import React from "react"
+import { Field, reduxForm } from "redux-form"
+import {
+  combineValidators,
+  composeValidators,
+  matchesField,
+  isRequired
+} from "revalidate"
+import {
+  Segment,
+  Header,
+  Form,
+  Divider,
+  Label,
+  Button,
+  Icon
+} from "semantic-ui-react"
 
-const AccountPage = () => <h1>Account Page</h1>
+import TextInput from "../../../app/common/form/TextInput"
 
-export default AccountPage
+const validate = combineValidators({
+  newPassword1: isRequired({ message: "Please enter a password" }),
+  newPassword2: composeValidators(
+    isRequired({ message: "Please confirm your password" }),
+    matchesField("newPassword1")({ message: "Passwords do not match" })
+  )()
+})
+
+const AccountPage = ({
+  error,
+  handleSubmit,
+  updatePassword,
+  providerId,
+  invalid,
+  submitting
+}) => (
+  <Segment>
+    <Header dividing size="large" content="Account" />
+    {providerId &&
+      providerId === "password" && (
+        <div>
+          <Header color="teal" sub content="Change password" />
+          <p>Use this form to update your account settings</p>
+          <Form onSubmit={handleSubmit(updatePassword)}>
+            <Field
+              width={8}
+              name="newPassword1"
+              type="password"
+              pointing="left"
+              inline={true}
+              component={TextInput}
+              basic={true}
+              placeholder="New Password"
+            />
+            <Field
+              width={8}
+              name="newPassword2"
+              type="password"
+              inline={true}
+              basic={true}
+              pointing="left"
+              component={TextInput}
+              placeholder="Confirm Password"
+            />
+            {error && (
+              <Label basic color="red">
+                {error}
+              </Label>
+            )}
+            <Divider />
+            <Button
+              disabled={invalid || submitting}
+              size="large"
+              positive
+              content="Update Password"
+            />
+          </Form>
+        </div>
+      )}
+
+    {providerId &&
+      providerId === "facebook.com" && (
+        <div>
+          <Header color="teal" sub content="Facebook Account" />
+          <p>Please visit Facebook to update your account settings</p>
+          <Button type="button" color="facebook">
+            <Icon name="facebook" />
+            Go to Facebook
+          </Button>
+        </div>
+      )}
+
+    {providerId &&
+      providerId === "google.com" && (
+        <div>
+          <Header color="teal" sub content="Google Account" />
+          <p>Please visit Google to update your account settings</p>
+          <Button type="button" color="google plus">
+            <Icon name="google plus" />
+            Go to Google
+          </Button>
+        </div>
+      )}
+  </Segment>
+)
+
+export default reduxForm({ form: "account", validate })(AccountPage)
