921a6435ee372b6f36dce948d441b763a650e944
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2018-05-29 21:50:48 +0600

    Add props to AccountPage

--- src/features/user/Settings/index.js
+++ src/features/user/Settings/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { connect } from "react-redux"
 import { Switch, Route, Redirect } from "react-router-dom"
 import { Grid } from "semantic-ui-react"
 
@@ -7,16 +8,25 @@ import AboutPage from "./AboutPage"
 import AccountPage from "./AccountPage"
 import PhotosPage from "./PhotosPage"
 import SettingsNav from "./SettingsNav"
+import { updatePassword } from "../../auth/authActions"
 
-const Settings = () => (
+const Settings = ({ updatePassword, providerId }) => (
   <Grid>
     <Grid.Column width={12}>
       <Switch>
         <Redirect exact from="/settings" to="/settings/basic" />
         <Route path="/settings/basic" component={BasicPage} />
         <Route path="/settings/about" component={AboutPage} />
-        <Route path="/settings/account" component={AccountPage} />
         <Route path="/settings/photos" component={PhotosPage} />
+        <Route
+          path="/settings/account"
+          render={() => (
+            <AccountPage
+              updatePassword={updatePassword}
+              providerId={providerId}
+            />
+          )}
+        />
       </Switch>
     </Grid.Column>
     <Grid.Column width={4}>
@@ -25,4 +35,8 @@ const Settings = () => (
   </Grid>
 )
 
-export default Settings
+const mapStateToProps = ({ firebase }) => ({
+  providerId: firebase.auth.providerData[0].providerId
+})
+
+export default connect(mapStateToProps, { updatePassword })(Settings)
