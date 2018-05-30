35b2e51436e0730885e429021d06caed50d45a4b
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2018-05-30 08:38:14 +0600

    Update eventData with user interests

--- src/app/data/eventData.js
+++ src/app/data/eventData.js
@@ -67,3 +67,12 @@ export const category = [
   { key: "music", text: "Music", value: "music" },
   { key: "travel", text: "Travel", value: "travel" }
 ]
+
+export const interests = [
+  { key: "drinks", text: "Drinks", value: "drinks" },
+  { key: "culture", text: "Culture", value: "culture" },
+  { key: "film", text: "Film", value: "film" },
+  { key: "food", text: "Food", value: "food" },
+  { key: "music", text: "Music", value: "music" },
+  { key: "travel", text: "Travel", value: "travel" }
+]
