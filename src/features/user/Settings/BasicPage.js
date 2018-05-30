a4afcea39bf9005de1770f2d92524006b7b5a66d
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2018-05-30 08:39:58 +0600

    Set updateOnDestroy to false for resucx-form

--- src/features/user/Settings/BasicPage.js
+++ src/features/user/Settings/BasicPage.js
@@ -1,5 +1,78 @@
-import React from "react"
+import React, { Component } from "react"
+import { Field, reduxForm } from "redux-form"
+import moment from "moment"
+import { Segment, Form, Header, Divider, Button } from "semantic-ui-react"
 
-const BasicPage = () => <h1>Basic Page</h1>
+import DateInput from "../../../app/common/form/DateInput"
+import PlaceInput from "../../../app/common/form/PlaceInput"
+import TextInput from "../../../app/common/form/TextInput"
+import RadioInput from "../../../app/common/form/RadioInput"
 
-export default BasicPage
+class BasicPage extends Component {
+  render() {
+    const { handleSubmit, updateProfile, pristine, submitting } = this.props
+
+    return (
+      <Segment>
+        <Header dividing size="large" content="Basics" />
+        <Form onSubmit={handleSubmit(updateProfile)}>
+          <Field
+            width={8}
+            name="displayName"
+            type="text"
+            component={TextInput}
+            placeholder="Known As"
+          />
+          <Form.Group inline>
+            <label>Gender: </label>
+            <Field
+              name="gender"
+              type="radio"
+              value="male"
+              label="Male"
+              component={RadioInput}
+            />
+            <Field
+              name="gender"
+              type="radio"
+              value="female"
+              label="Female"
+              component={RadioInput}
+            />
+          </Form.Group>
+          <Field
+            width={8}
+            name="dateOfBirth"
+            component={DateInput}
+            placeholder="Date of Birth"
+            dateFormat="YYYY-MM-DD"
+            showYearDropdown={true}
+            showMonthDropdown={true}
+            dropdownMode="select"
+            maxDate={moment().subtract(18, "years")}
+          />
+          <Field
+            name="city"
+            placeholder="Home Town"
+            options={{ types: ["(cities)"] }}
+            component={PlaceInput}
+            width={8}
+          />
+          <Divider />
+          <Button
+            disabled={pristine || submitting}
+            size="large"
+            positive
+            content="Update Profile"
+          />
+        </Form>
+      </Segment>
+    )
+  }
+}
+
+export default reduxForm({
+  form: "userProfile",
+  enableReinitialize: true,
+  destroyOnUnmount: false
+})(BasicPage)
