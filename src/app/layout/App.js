dff8da809a7fb58e4c6c12e83e21da8c8ef9bec1
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2018-06-02 20:34:19 +0600

    Remove test component routes

--- src/app/layout/App.js
+++ src/app/layout/App.js
@@ -9,11 +9,11 @@ import PeopleDashboard from "../../features/user/PeopleDashboard"
 import UserDetail from "../../features/user/UserDetail"
 import Settings from "../../features/user/Settings"
 import NavBar from "../../features/nav/NavBar"
-import EventForm from "../../features/events/EventForm"
+import CreateEventForm from "../../features/events/EventForm/CreateEventForm"
+import ManageEventForm from "../../features/events/EventForm/ManageEventForm"
 import ModalManager from "../../features/modals/ModalManager"
 import NotFound from "./NotFound"
-
-import TestComponent from "../../features/testArea/TestComponent"
+// import TestComponent from "../../features/testArea/TestComponent"
 
 const App = () => (
   <div>
@@ -31,12 +31,12 @@ const App = () => (
             <Switch>
               <Route path="/events" component={EventDashboard} />
               <Route path="/event/:id" component={EventDetail} />
-              <Route path="/manage/:id" component={EventForm} />
+              <Route path="/manage/:id" component={ManageEventForm} />
               <Route path="/people" component={PeopleDashboard} />
               <Route path="/profile/:id" component={UserDetail} />
               <Route path="/settings" component={Settings} />
-              <Route path="/createEvent" component={EventForm} />
-              <Route path="/test" component={TestComponent} />
+              <Route path="/createEvent" component={CreateEventForm} />
+              {/* <Route path="/test" component={TestComponent} /> */}
               <Route component={NotFound} />
             </Switch>
           </Container>
