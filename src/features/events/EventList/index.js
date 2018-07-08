b86b716f695323adc81fbe0125d29e1ac521d2b6
kukiron <kafiluddin.kiron@gmail.com>
2018-07-08 20:55:31 +0600

    Enable infinite scrolling

--- src/features/events/EventList/index.js
+++ src/features/events/EventList/index.js
@@ -1,16 +1,23 @@
 import React from "react"
 import EventListItem from "./EventListItem"
+import InfiniteScroll from "react-infinite-scroller"
 
-const EventList = ({ events, onDeleteEvent }) => (
+const renderEvents = events =>
+  events && events.map(event => <EventListItem key={event.id} event={event} />)
+
+const EventList = ({ events, loading, getNextEvents, moreEvents }) => (
   <div>
     {events &&
-      events.map(event => (
-        <EventListItem
-          key={event.id}
-          event={event}
-          onDeleteEvent={onDeleteEvent}
-        />
-      ))}
+      events.length > 0 && (
+        <InfiniteScroll
+          pageStart={0}
+          loadMore={getNextEvents}
+          hasMore={!loading && moreEvents}
+          initialLoad={false}
+        >
+          {renderEvents(events)}
+        </InfiniteScroll>
+      )}
   </div>
 )
 
