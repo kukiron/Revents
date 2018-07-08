61592bcd920bc67664b85bcf7c614999e0677e07
kukiron <kafiluddin.kiron@gmail.com>
2018-07-09 04:07:01 +0600

    Add action for handling addition of comments

--- src/features/events/eventActions.js
+++ src/features/events/eventActions.js
@@ -124,3 +124,29 @@ export const getEventsForDashboard = lastEvent => async (
     dispatch(asyncActionError())
   }
 }
+
+export const addEventComment = (eventId, values, parentId) => async (
+  dispatch,
+  getState,
+  { getFirebase }
+) => {
+  const firebase = getFirebase()
+  const profile = getState().firebase.profile
+  const user = firebase.auth().currentUser
+
+  let newComment = {
+    parentId,
+    displayName: profile.displayName,
+    photoURL: profile.photoURL || "/assets/user.png",
+    uid: user.uid,
+    text: values.comment,
+    date: Date.now()
+  }
+
+  try {
+    await firebase.push(`event_chat/${eventId}`, newComment)
+  } catch (error) {
+    console.log(error)
+    toastr.error("Oops!", "Problem adding comment")
+  }
+}
