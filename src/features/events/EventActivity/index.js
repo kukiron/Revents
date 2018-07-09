8f6c514a4d2f377ee0bccf9d80a1ca1e6a652b66
kukiron <kafiluddin.kiron@gmail.com>
2018-07-09 15:51:53 +0600

    Show event activity to right side

--- src/features/events/EventActivity/index.js
+++ src/features/events/EventActivity/index.js
@@ -1,13 +1,20 @@
 import React from "react"
-import { Header, Segment } from "semantic-ui-react"
+import { Header, Segment, Feed, Sticky } from "semantic-ui-react"
 
-const EventActivity = () => (
-  <div>
+import EventActivityItem from "./EventActivityItem"
+
+const EventActivity = ({ activities, contextRef }) => (
+  <Sticky context={contextRef} offset={100}>
     <Header attached="top" content="Recent Activity" />
     <Segment attached>
-      <p>Recent User Activity</p>
+      <Feed>
+        {activities &&
+          activities.map(activity => (
+            <EventActivityItem key={activity.id} activity={activity} />
+          ))}
+      </Feed>
     </Segment>
-  </div>
+  </Sticky>
 )
 
 export default EventActivity
