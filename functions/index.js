0a0549f773bf7d09827266dbebb659fd58d71b3b
kukiron <kafiluddin.kiron@gmail.com>
2018-07-12 03:45:36 +0600

    Add firebase cloud functions for following & unfollowing users

--- functions/index.js
+++ functions/index.js
@@ -54,3 +54,47 @@ exports.cancelActivity = functions.firestore
 
     return addActivityToCollection(activity)
   })
+
+exports.followUser = functions.firestore
+  .document("users/{followerUid}/following/{followingUid}")
+  .onCreate((event, context) => {
+    const followerUid = context.params.followerUid
+    const followingUid = context.params.followingUid
+
+    const followerDoc = admin
+      .firestore()
+      .collection("users")
+      .doc(followerUid)
+
+    return followerDoc.get().then(doc => {
+      let userData = doc.data()
+      const { displayName, city, photoURL } = userData
+      const follower = {
+        displayName,
+        photoURL: photoURL || "/assets/user.png",
+        city: city || "Unknown city"
+      }
+
+      return admin
+        .firestore()
+        .collection("users")
+        .doc(followingUid)
+        .collection("followers")
+        .doc(followerUid)
+        .set(follower)
+    })
+  })
+
+exports.unfollowUser = functions.firestore
+  .document("users/{followerUid}/following/{followingUid}")
+  .onDelete((event, context) =>
+    admin
+      .firestore()
+      .collection("users")
+      .doc(context.params.followingUid)
+      .collection("following")
+      .doc(context.params.followerUid)
+      .delete()
+      .then(() => console.log("doc deleted"))
+      .catch(err => console.log(err))
+  )
