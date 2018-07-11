fea370b8f3d5c1b970803fc882565f0de20f4e63
kukiron <kafiluddin.kiron@gmail.com>
2018-07-12 03:40:04 +0600

    Dynamic button loading based on user status

--- src/features/user/UserDetail/UserDetailSidebar.js
+++ src/features/user/UserDetail/UserDetailSidebar.js
@@ -2,7 +2,13 @@ import React from "react"
 import { Link } from "react-router-dom"
 import { Grid, Segment, Button } from "semantic-ui-react"
 
-const UserDetailSidebar = ({ isCurrentUser, profile }) => (
+const UserDetailSidebar = ({
+  isCurrentUser,
+  profile,
+  isFollowing,
+  followUser,
+  unfollowUser
+}) => (
   <Grid.Column width={4}>
     <Segment>
       {isCurrentUser ? (
@@ -14,12 +20,21 @@ const UserDetailSidebar = ({ isCurrentUser, profile }) => (
           basic
           content="Edit Profile"
         />
+      ) : isFollowing ? (
+        <Button
+          color="teal"
+          fluid
+          basic
+          content="Unfollow"
+          onClick={() => unfollowUser(profile)}
+        />
       ) : (
         <Button
           color="teal"
           fluid
           basic
           content={`Follow ${profile.displayName}`}
+          onClick={() => followUser(profile)}
         />
       )}
     </Segment>
