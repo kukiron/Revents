002ce39dec0ed1518cbb6dfc9c1b9f9c3db87ba7
kukiron <kafiluddin.kiron@gmail.com>
2018-07-13 02:59:36 +0600

    Add check for event existence

--- src/features/events/EventDetail/index.js
+++ src/features/events/EventDetail/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react"
 import { connect } from "react-redux"
 import { compose } from "redux"
+import { toastr } from "react-redux-toastr"
 import { withFirestore, firebaseConnect, isEmpty } from "react-redux-firebase"
 import { Grid } from "semantic-ui-react"
 
@@ -8,6 +9,7 @@ import EventDetailHeader from "./EventDetailHeader"
 import EventDetailInfo from "./EventDetailInfo"
 import EventDetailChat from "./EventDetailChat"
 import EventDetailSidebar from "./EventDetailSidebar"
+import LoadingSpinner from "../../../app/layout/LoadingSpinner"
 import { objToArr, createDataTree } from "../../../app/common/utils/helpers"
 import { goingToEvent, cancellGoingToEvent } from "../../user/userActions"
 import { addEventComment } from "../eventActions"
@@ -21,9 +23,17 @@ const actions = {
 }
 
 class EventDetail extends Component {
+  state = { initialLoading: true }
+
   async componentDidMount() {
     const { firestore, match } = this.props
+    const event = await firestore.get(`events/${match.params.id}`)
+    if (!event.exists) {
+      toastr.error("Not found", "This isn't the event you're looking for")
+      this.props.history.push("/error")
+    }
     await firestore.setListener(`events/${match.params.id}`)
+    this.setState({ initialLoading: false })
   }
 
   async componentWillUnmount() {
@@ -35,18 +45,27 @@ class EventDetail extends Component {
     const {
       auth,
       event,
+      match,
       loading,
+      requesting,
       goingToEvent,
       cancellGoingToEvent,
       addEventComment,
       eventChat,
       openModal
     } = this.props
-    const attendees = event && event.attendees && objToArr(event.attendees)
+    const attendees =
+      event &&
+      event.attendees &&
+      objToArr(event.attendees).sort((a, b) => a.joinDate - b.joinDate)
     const isHost = event.hostUid === auth.uid
     const isGoing = attendees && attendees.some(a => a.id === auth.uid)
     const commentTree = !isEmpty(eventChat) && createDataTree(eventChat)
     const authenticated = auth.isLoaded && !auth.isEmpty
+    const loadingEvent = requesting[`events/${match.params.id}`]
+
+    if (loadingEvent || this.state.initialLoading)
+      return <LoadingSpinner inverted={true} />
 
     return (
       <Grid>
@@ -79,11 +98,12 @@ class EventDetail extends Component {
 }
 
 const mapStateToProps = (
-  { firestore: { ordered }, firebase: { auth, data }, async },
+  { firestore: { ordered, status }, firebase: { auth, data }, async },
   { match }
 ) => ({
   auth,
   loading: async.loading,
+  requesting: status.requesting,
   event: (ordered.events && ordered.events[0]) || {},
   eventChat:
     !isEmpty(data.event_chat) && objToArr(data.event_chat[match.params.id])
@@ -92,5 +112,8 @@ const mapStateToProps = (
 export default compose(
   withFirestore,
   connect(mapStateToProps, actions),
-  firebaseConnect(({ match }) => [`event_chat/${match.params.id}`])
+  firebaseConnect(
+    ({ auth, match }) =>
+      auth.isLoaded && !auth.isEmpty && [`event_chat/${match.params.id}`]
+  )
 )(EventDetail)
