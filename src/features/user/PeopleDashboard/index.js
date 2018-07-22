a296bada5232c60683529e400426934cc9ec2a65
kukiron <kafiluddin.kiron@gmail.com>
2018-07-22 20:09:56 +0600

    Populate empty people card with placeholder text

--- src/features/user/PeopleDashboard/index.js
+++ src/features/user/PeopleDashboard/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import { connect } from "react-redux"
 import { compose } from "redux"
 import { firestoreConnect } from "react-redux-firebase"
-import { Grid, Segment, Header, Card } from "semantic-ui-react"
+import { Grid, Segment, Header, Card, Icon } from "semantic-ui-react"
 
 import PersonCard from "./PersonCard"
 
@@ -21,32 +21,51 @@ const query = ({ auth }) => [
   }
 ]
 
-const PeopleDashboard = ({ followings, followers }) => {
-  return (
-    <Grid>
-      <Grid.Column width={16}>
-        <Segment>
-          <Header dividing content="People following me" />
-          <Card.Group itemsPerRow={8} stackable>
-            {followers &&
+const renderEmptyCard = () => (
+  <Header as="h3" icon textAlign="center">
+    <Icon name="users" />
+    <Header.Content>There's no one to show</Header.Content>
+  </Header>
+)
+
+const PeopleDashboard = ({ followings, followers }) => (
+  <Grid>
+    <Grid.Column width={16}>
+      <Segment>
+        <Header dividing content="People following me" />
+        <Card.Group itemsPerRow={8} stackable>
+          {followers ? (
+            followers.length > 0 ? (
               followers.map(follower => (
                 <PersonCard key={follower.id} user={follower} />
-              ))}
-          </Card.Group>
-        </Segment>
-        <Segment>
-          <Header dividing content="People I am following" />
-          <Card.Group itemsPerRow={8} stackable>
-            {followers &&
+              ))
+            ) : (
+              renderEmptyCard()
+            )
+          ) : (
+            <h4>Loading...</h4>
+          )}
+        </Card.Group>
+      </Segment>
+      <Segment>
+        <Header dividing content="People I am following" />
+        <Card.Group itemsPerRow={8} stackable>
+          {followings ? (
+            followings.length > 0 ? (
               followings.map(following => (
                 <PersonCard key={following.id} user={following} />
-              ))}
-          </Card.Group>
-        </Segment>
-      </Grid.Column>
-    </Grid>
-  )
-}
+              ))
+            ) : (
+              renderEmptyCard()
+            )
+          ) : (
+            <h4>Loading...</h4>
+          )}
+        </Card.Group>
+      </Segment>
+    </Grid.Column>
+  </Grid>
+)
 
 const mapStateToProps = ({ firebase, firestore }) => ({
   followings: firestore.ordered.following,
