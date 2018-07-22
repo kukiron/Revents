48e1bfa97d31cc494b2ff3a4064debb39806d8f0
kukiron <kafiluddin.kiron@gmail.com>
2018-07-22 17:12:14 +0600

    Refactor config keys object

--- src/app/config/keys/index.js
+++ src/app/config/keys/index.js
@@ -1,7 +1,7 @@
-const googleApiKey = `${process.env.REACT_APP_GOOGLE_API_KEY}`
-const firebaseApiKey = `${process.env.REACT_APP_FIREBASE_CONFIG_apiKey}`
-const messagingSenderId = `${
-  process.env.REACT_APP_FIREBASE_CONFIG_messagingSenderId
-}`
-
-module.exports = { googleApiKey, firebaseApiKey, messagingSenderId }
+module.exports = {
+  googleApiKey: `${process.env.REACT_APP_GOOGLE_API_KEY}`,
+  firebaseApiKey: `${process.env.REACT_APP_FIREBASE_CONFIG_apiKey}`,
+  messagingSenderId: `${
+    process.env.REACT_APP_FIREBASE_CONFIG_messagingSenderId
+  }`
+}
